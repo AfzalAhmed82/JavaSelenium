7761d2203d14f7c70930b2244b3f731bee12d0cd
AfzalAhmed82 <emailafzalahmed@gmail.com>
2022-11-09 21:15:19 +0530

    added comment

--- src/test/java/day2/Demo.java
+++ src/test/java/day2/Demo.java
@@ -2,7 +2,7 @@ package day2;
 
 public class Demo {
     public static void main(String[] args) {
-
+    // adding comments for git test
         System.out.println("adding files to git");
     }
 }
